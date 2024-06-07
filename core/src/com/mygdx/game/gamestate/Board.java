fbb3258ffd2527fd0e6359e26269d64e6ac6d5eb
ChristopherJ123 <christopherz2002@hotmail.com>
2024-06-08 02:33:10 +0700

    Updated Board.java to show CurrentPiece in the board

---
+++ core/src/com/mygdx/game/gamestate/Board.java
@@ -0,0 +1,42 @@
+package com.mygdx.game.gamestate;
+
+import com.mygdx.game.config.Config;
+import com.mygdx.game.tetromino.Block;
+import com.mygdx.game.tetromino.EmptyBlock;
+
+public class Board {
+    private Block[][] playfield;
+
+    public Board() {
+        playfield = new Block[Config.BOARD_HEIGHT + Config.BOARD_BUFFER_ZONE][Config.BOARD_WIDTH];
+
+        for (int i = 0; i < playfield.length; i++) {
+            for (int j = 0; j < playfield[i].length; j++) {
+                playfield[i][j] = new EmptyBlock();
+            }
+        }
+    }
+
+    public void addCurrentPiece(CurrentPiece currentPiece) {
+        for (int i = 0; i < playfield.length; i++) {
+            for (int j = 0; j < playfield[i].length; j++) {
+                playfield[i][j] = new EmptyBlock();
+            }
+        }
+        for (int i = 0; i < currentPiece.getTetromino().getShapeBoolean().length; i++) {
+            for (int j = 0; j < currentPiece.getTetromino().getShapeBoolean()[i].length; j++) {
+                if (currentPiece.getTetromino().getShapeBoolean()[i][j]) {
+                    playfield[currentPiece.getY()+(currentPiece.getTetromino().getShapeBoolean().length - 1 - i)][currentPiece.getX()+j] = new Block(currentPiece.getTetromino().getColor(), true, false);
+                }
+            }
+        }
+    }
+
+    public Block[][] getPlayfield() {
+        return playfield;
+    }
+
+    public void setPlayfield(Block[][] playfield) {
+        this.playfield = playfield;
+    }
+}
